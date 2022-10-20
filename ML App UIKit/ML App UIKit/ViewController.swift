3841c10977e650d28022511812758c314fd4f312
PeterChav01 <shadow_nfsc@yahoo.com.mx>
2022-10-20 11:21:33 -0500

    Project Setup

---
+++ ML App UIKit/ML App UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ML App UIKit
+//
+//  Created by Jose Chavez Aparicio on 20/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
