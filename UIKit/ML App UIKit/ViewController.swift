df45810b3238f69148d536b3ed2485ee3cde8ca9
PeterChav01 <shadow_nfsc@yahoo.com.mx>
2022-10-24 15:30:53 -0500

    Added initial screen design, Code implemented to add mock information to the Table View

--- UIKit/ML App UIKit/ViewController.swift
+++ UIKit/ML App UIKit/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let products = [
+        "iPhone",
+        "Ipad",
+        "Apple Watch",
+        "iMac",
+        "MacBook"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(products[indexPath.row])
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = products[indexPath.row]
+        
+        return  cell
+    }
+}
